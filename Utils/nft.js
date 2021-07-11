e02dca96f6b4b34ce5171b8643fb9a738e95818b
Dogu <dogudeniz.ugur@gmail.com>
2021-07-11 07:54:30 +0300

    New wallet creation

---
+++ Utils/nft.js
@@ -0,0 +1,12 @@
+import {Web3} from 'web3-react-native';
+
+(async () => {
+  const password =
+    '*xH@ak46%PyWJ^TSu9sMmyYe@d*nMpCPqf2^VMHwRTXR5@%C*tvPYKv%$5zYN7jbs!zv*YaBY%FFeYtejYu@FjsZvGyT3z#HhE@';
+  const {Keystore, Wallet} = await Web3(
+    'https://ropsten.infura.io/v3/d93ebd2ea335456fab8d17648bcc924f',
+  );
+  const ks = await Keystore.create(password);
+  // Here, we generate a new wallet using the generated keystore.
+  const {address, sendFunds} = await Wallet.create(ks, password);
+})();
